c1cb6432ca053dfce97cdde1c4732cbfae6f2fb5
SonajiRao <sonajipawar9497@gmail.com>
2026-01-12 21:06:11 +0530

    Add expense tracker application

--- app.py
+++ app.py
@@ -1,24 +1,50 @@
-import requests
+import pandas as pd
+from datetime import date
 
-API_KEY = "YOUR_API_KEY_HERE"
+FILE = "expenses.csv"
 
-city = input("Enter city name: ")
+def add_expense():
+    category = input("Category (Food, Travel, etc): ")
+    amount = float(input("Amount: "))
+    note = input("Note: ")
 
-url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    new_data = {
+        "Date": date.today(),
+        "Category": category,
+        "Amount": amount,
+        "Note": note
+    }
 
-response = requests.get(url)
-data = response.json()
+    df = pd.read_csv(FILE)
+    df = pd.concat([df, pd.DataFrame([new_data])], ignore_index=True)
+    df.to_csv(FILE, index=False)
 
-if data["cod"] != 200:
-    print("City not found!")
-else:
-    temp = data["main"]["temp"]
-    humidity = data["main"]["humidity"]
-    condition = data["weather"][0]["description"]
+    print("Expense added successfully!\n")
 
-    print("\nWeather Report")
-    print("------------------")
-    print(f"City: {city}")
-    print(f"Temperature: {temp} °C")
-    print(f"Humidity: {humidity}%")
-    print(f"Condition: {condition.capitalize()}")
+def show_summary():
+    df = pd.read_csv(FILE)
+
+    print("\nTotal Expense:", df["Amount"].sum())
+
+    print("\nCategory-wise Summary:")
+    print(df.groupby("Category")["Amount"].sum())
+
+    print("\nHighest Expense:")
+    print(df.loc[df["Amount"].idxmax()])
+
+while True:
+    print("\nExpense Tracker")
+    print("1. Add Expense")
+    print("2. View Summary")
+    print("3. Exit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        add_expense()
+    elif choice == "2":
+        show_summary()
+    elif choice == "3":
+        break
+    else:
+        print("Invalid choice")
